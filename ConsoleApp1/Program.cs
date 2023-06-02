655001591b2cb68bdfcd03e0a3d05c8478e99eb1
VaDimk12 <vadimkomis6@gmail.com>
2023-06-02 12:06:31 +0300

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int prev = 0;
+            int yet = 1;
+            int n = 40;
+            for(int i = 2; i < n; i++)
+            {
+                int next = prev + yet;
+                Console.WriteLine(next);
+
+                prev = yet;
+                yet = next;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
